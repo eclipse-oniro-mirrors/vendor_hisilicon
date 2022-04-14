910a9a35ad392c224d5115f1d529b9d124ced89a
song_qianqian <song_qianqian1@hoperun.com>
2022-04-14 20:40:40 +0800

    add robotdemo
    Signed-off-by: song_qianqian <song_qianqian1@hoperun.com>

---
+++ hispark_pegasus/demo/robotdemo/robot_car/ssd1306/ssd1306_conf.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022 HiHope Open Source Organization .
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *
+ * limitations under the License.
+ */
+/**
+ * Private configuration file for the SSD1306 library.
+ * This example is configured for STM32F0, I2C and including all fonts.
+ */
+
+#ifndef __SSD1306_CONF_H__
+#define __SSD1306_CONF_H__
+
+// Choose a bus
+#define SSD1306_USE_I2C
+
+// I2C Configuration
+#define SSD1306_I2C_ADDR        (0x3C << 1)
+
+// Include only needed fonts
+#define SSD1306_INCLUDE_FONT_6x8
+#define SSD1306_INCLUDE_FONT_7x10
+#define SSD1306_INCLUDE_FONT_11x18
+#define SSD1306_INCLUDE_FONT_16x26
+
+#endif /* __SSD1306_CONF_H__ */
