c4fae7e7d928360019598aa9be3c9b546d430d91
lianzhian <lian_zhian@hoperun.com>
2022-06-07 17:06:07 +0800

    新增3861样例供开发者参考
    Signed-off-by: lianzhian <lian_zhian@hoperun.com>

---
+++ hispark_pegasus/demo/colorfullight_demo/README.md
@@ -0,0 +1,71 @@
+# HiSpark WiFi-IoT 套件样例开发--colorful_light_demo
+
+![hihope_illustration](https://gitee.com/hihopeorg/hispark-hm-pegasus/raw/master/docs/figures/hihope_illustration.png)
+
+[HiSpark WiFi-IoT开发套件](https://item.taobao.com/item.htm?spm=a1z10.1-c-s.w5003-23341819265.1.bf644a82Da9PZK&id=622343426064&scene=taobao_shop) 首发于HDC 2020，是首批支持OpenHarmony 2.0的开发套件，亦是官方推荐套件，由润和软件HiHope量身打造，已在OpenHarmony社区和广大OpenHarmony开发者中得到广泛应用。
+
+![wifi_iot](https://gitee.com/hihopeorg/hispark-hm-pegasus/raw/master/docs/figures/2.png)
+
+
+
+## 一、炫彩灯板上外设与主控芯片（Pegasus）引脚的对应关系
+
+* 三色LED——PWM占空比控制颜色
+  - GPIO10\PWM1: 红
+  - GPIO11\PWM2: 绿
+  - GPIO12\pwm3: 蓝
+* 光敏电阻——ADC采集值和电压对应，电压和电阻对应，电阻和光照强度对应
+  - GPIO09: ADC4
+* 人体红外传感器——ADC采集值反应是否有人靠近
+  - GPIO07: ADC3
+
+
+
+## 二、如何编译
+
+1. 将此目录下的 `colorful_light_demo.c` 和 `BUILD.gn` 复制到openharmony源码的`applications\sample\wifi-iot\app\iothardware`目录下，
+2. 修改openharmony源码的`applications\sample\wifi-iot\app\BUILD.gn`文件，将其中的 `features` 改为：
+```python
+    features = [
+        "iothardware:colorful_light_demo",
+    ]
+```
+3. 在openharmony源码顶层目录执行：`python build.py wifiiot`
+
+### 报错解决
+
+1. 编译过程中报错：undefined reference to `hi_pwm_init` 等几个 `hi_pwm_`开头的函数，
+	* **原因：** 因为默认情况下，hi3861_sdk中，PWM的CONFIG选项没有打开
+	* **解决：** 修改`vendor\hisi\hi3861\hi3861\build\config\usr_config.mk`文件中的`CONFIG_PWM_SUPPORT`行：
+	  * `# CONFIG_PWM_SUPPORT is not set`修改为`CONFIG_PWM_SUPPORT=y`
+
+## 三、运行结果
+
+烧录文件后，按下reset按键，程序开始运行：
+
+* 炫彩灯板的三色灯会先依次按照红绿蓝三色闪烁两次
+
+* 三色灯会呈现蓝色，亮度变化
+
+* 循环：三色灯会分别呈现红色、绿色，亮度变化
+
+  
+
+### 【套件支持】
+
+##### 1. 套件介绍  http://www.hihope.org/pro/pro1.aspx?mtt=8
+
+##### 2. 套件购买  https://item.taobao.com/item.htm?id=622343426064&scene=taobao_shop
+
+##### 3. 技术资料
+
+- Gitee码云网站（OpenHarmony Sample Code等) **https://gitee.com/hihopeorg**
+
+- HiHope官网-资源中心（SDK包、技术文档下载）[**www.hihope.org**](http://www.hihope.org/)
+
+##### 4. 互动交流
+
+- 润和HiHope技术交流-微信群（加群管理员微信13605188699，发送文字#申请加入润和官方群#，予以邀请入群）
+- HiHope开发者社区-论坛 **https://bbs.elecfans.com/group_1429**
+- 润和HiHope售后服务群（QQ：980599547）
+- 售后服务电话（025-52668590）
---
+++ hispark_pegasus/demo/colorfullight_demo/colorful_light_demo.c
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2022 HiHope Open Source Organization .
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http:// www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "ohos_init.h"
+#include "cmsis_os2.h"
+#include "wifiiot_gpio.h"
+#include "wifiiot_gpio_ex.h"
+#include "wifiiot_pwm.h"
+#include "wifiiot_adc.h"
+#include "wifiiot_errno.h"
+
+#define HUMAN_SENSOR_CHAN_NAME WIFI_IOT_ADC_CHANNEL_3
+#define LIGHT_SENSOR_CHAN_NAME WIFI_IOT_ADC_CHANNEL_4
+
+#define RED_LED_PIN_NAME WIFI_IOT_IO_NAME_GPIO_10
+#define RED_LED_PIN_FUNCTION WIFI_IOT_IO_FUNC_GPIO_10_GPIO
+
+#define GREEN_LED_PIN_NAME WIFI_IOT_IO_NAME_GPIO_11
+#define GREEN_LED_PIN_FUNCTION WIFI_IOT_IO_FUNC_GPIO_11_GPIO
+
+#define BLUE_LED_PIN_NAME WIFI_IOT_IO_NAME_GPIO_12
+#define BLUE_LED_PIN_FUNCTION WIFI_IOT_IO_FUNC_GPIO_12_GPIO
+
+#define LED_DELAY_TIME_US 300000
+#define LED_BRIGHT WIFI_IOT_GPIO_VALUE1
+#define LED_DARK WIFI_IOT_GPIO_VALUE0
+
+#define NUM_LEDS 3
+#define NUM_BLINKS 2
+#define NUM_SENSORS 2
+
+#define ADC_RESOLUTION 4096
+#define PWM_FREQ_DIVITION 64000
+#define ATTR.STACK_SIZE 4096
+#define TEN_THOUSAND 10000
+#define TWO_HUNDRED_AND_FIFTY_THOUSAND 250000
+#define TWO 2
+
+
+static void CorlorfulLightTask(int *arg)
+{
+    (void)arg;
+    static const WifiIotGpioIdx pins[] = {RED_LED_PIN_NAME, GREEN_LED_PIN_NAME, BLUE_LED_PIN_NAME};
+
+    for (int i = 0; i < NUM_BLINKS; i++) {
+        for (unsigned j = 0; j < sizeof(pins)/sizeof(pins[0]); j++) {
+            GpioSetOutputVal(pins[j], LED_BRIGHT);
+            usleep(LED_DELAY_TIME_US);
+            GpioSetOutputVal(pins[j], LED_DARK);
+            usleep(LED_DELAY_TIME_US);
+        }
+    }
+
+    IoSetFunc(RED_LED_PIN_NAME, WIFI_IOT_IO_FUNC_GPIO_10_PWM1_OUT);
+    IoSetFunc(GREEN_LED_PIN_NAME, WIFI_IOT_IO_FUNC_GPIO_11_PWM2_OUT);
+    IoSetFunc(BLUE_LED_PIN_NAME, WIFI_IOT_IO_FUNC_GPIO_12_PWM3_OUT);
+
+    PwmInit(WIFI_IOT_PWM_PORT_PWM1); // R
+    PwmInit(WIFI_IOT_PWM_PORT_PWM2); // G
+    PwmInit(WIFI_IOT_PWM_PORT_PWM3); // B
+
+    // use PWM control BLUE LED brightness
+    for (int i = 1; i <= ADC_RESOLUTION; i *= TWO) {
+        PwmStart(WIFI_IOT_PWM_PORT_PWM3, i, PWM_FREQ_DIVITION);
+        usleep(TWO_HUNDRED_AND_FIFTY_THOUSAND);
+        PwmStop(WIFI_IOT_PWM_PORT_PWM3);
+    }
+
+    while (1) {
+        unsigned short duty[NUM_SENSORS] = {0, 0};
+        unsigned short data[NUM_SENSORS] = {0, 0};
+        static const WifiIotAdcChannelIndex chan[] = {HUMAN_SENSOR_CHAN_NAME, LIGHT_SENSOR_CHAN_NAME};
+        static const WifiIotPwmPort port[] = {WIFI_IOT_PWM_PORT_PWM1, WIFI_IOT_PWM_PORT_PWM2};
+
+        for (size_t i = 0; i < sizeof(chan)/sizeof(chan[0]); i++) {
+            if (AdcRead(chan[i], &data[i], WIFI_IOT_ADC_EQU_MODEL_4, WIFI_IOT_ADC_CUR_BAIS_DEFAULT, 0)
+                == WIFI_IOT_SUCCESS) {
+                duty[i] = PWM_FREQ_DIVITION * (unsigned int)data[i] / ADC_RESOLUTION;
+            }
+            PwmStart(port[i], duty[i], PWM_FREQ_DIVITION);
+            usleep(TEN_THOUSAND);
+            PwmStop(port[i]);
+        }
+    }
+}
+
+static void ColorfulLightDemo(void)
+{
+    osThreadAttr_t attr;
+
+    GpioInit();
+
+    // set Red/Green/Blue LED pin to GPIO function
+    IoSetFunc(RED_LED_PIN_NAME, RED_LED_PIN_FUNCTION);
+    IoSetFunc(GREEN_LED_PIN_NAME, GREEN_LED_PIN_FUNCTION);
+    IoSetFunc(BLUE_LED_PIN_NAME, BLUE_LED_PIN_FUNCTION);
+
+    // set Red/Green/Blue LED pin as output
+    GpioSetDir(RED_LED_PIN_NAME, WIFI_IOT_GPIO_DIR_OUT);
+    GpioSetDir(GREEN_LED_PIN_NAME, WIFI_IOT_GPIO_DIR_OUT);
+    GpioSetDir(BLUE_LED_PIN_NAME, WIFI_IOT_GPIO_DIR_OUT);
+
+    attr.name = "CorlorfulLightTask";
+    attr.attr_bits = 0U;
+    attr.cb_mem = NULL;
+    attr.cb_size = 0U;
+    attr.stack_mem = NULL;
+    attr.stack_size = ATTR.STACK_SIZE;
+    attr.priority = osPriorityNormal;
+
+    if (osThreadNew(CorlorfulLightTask, NULL, &attr) == NULL) {
+        printf("[ColorfulLightDemo] Falied to create CorlorfulLightTask!\n");
+    }
+}
+
+APP_FEATURE_INIT(ColorfulLightDemo);
