b43109f849d04e6429a8919adeb5308aaca9d9eb
志超周 <10663331+zhi-chaozhou@user.noreply.gitee.com>
2022-07-04 12:16:13 +0000

    推荐time相关代码注释 Signed-off-by:zhouzhichao<480678944@qq.com>

--- hispark_pegasus/demo/time_demo/timer.c
+++ hispark_pegasus/demo/time_demo/timer.c
@@ -3,7 +3,12 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *     http:// www.apache.org/licenses/LICENSE-2.0
  *
+ * 版权所有 （C） 2022 HiHope 开源组织 。
+ * 根据 Apache 许可证 2.0 版（“许可证”）进行许可;
+ * 除非符合许可证，否则您不得使用此文件。
+ * 您可以在以下位置获取许可证副本：
  *     http:// www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,6 +18,12 @@
  *
  * limitations under the License.
  */
+ * 除非适用法律要求或书面同意，否则软件
+ * 根据许可证分发的按“原样”分发，
+ * 不提供任何明示或暗示的保证或条件。
+ * 请参阅许可证，了解管理权限和
+ *
+ * 许可证下的限制。
 
 #include <stdio.h>
 #include <unistd.h>
